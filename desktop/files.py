7681d952225544c847f19af3a883cf60ffff559d
HeIIow2 <Lars.Noack@outlook.de>
2022-06-02 18:59:06 +0200

    started a new desktop programm

--- desktop/files.py
+++ desktop/files.py
@@ -1,10 +1,13 @@
 import os
 import os.path
+import requests
+import json
 
 FILE_PATH = "./external_files/"
 TEMP_PATH = "./temp/"
 
 CONFIG_FILE = "config.json"
+CONFIG_URL = "https://raw.githubusercontent.com/HeIIow2/Neko/master/desktop/external_files/config.json"
 
 if not os.path.exists(FILE_PATH):
     raise Exception("File path does not exist")
@@ -16,7 +19,23 @@ class Config:
     def __init__(self):
         self.path = os.path.join(FILE_PATH, CONFIG_FILE)
         if not os.path.exists(self.path):
-            pass
+            print("Downloading config file...")
+            self.download_config()
 
-    def download_config(self, url):
-        pass
+        with open(self.path, "r") as f:
+            self.config = json.load(f)
+
+        self.app_title = self.config["app-title"]
+        if self.config["window-maximized"]:
+            self.window_state = "zoomed"
+        else:
+            self.window_state = "normal"
+        self.window_geometry = f"{self.config['window-dimensions'][0]}x{self.config['window-dimensions'][1]}"
+
+    def download_config(self):
+        r = requests.get(CONFIG_URL)
+        if r.status_code != 200:
+            raise Exception("Config file could not be downloaded")
+
+        with open(self.path, "w") as f:
+            f.write(r.text)
--- desktop/main.py
+++ desktop/main.py
@@ -2,7 +2,11 @@ import files
 
 import tkinter as tk
 
+config = files.Config()
+
 if __name__ == '__main__':
     root = tk.Tk()
-    root.title("NEKOS!!!")
+    root.title(config.app_title)
+    root.geometry(config.window_geometry)
+    root.state(config.window_state)
     root.mainloop()
